067414831e2a867a12ea2675e3b186ad3790eaeb
robingiraud <robin.giraud5@gmail.com>
2020-04-27 13:45:18 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cdiscount-robingiraud", # Replace with your own username
+    version="0.0.1",
+    author="Robin Giraud",
+    author_email="robin.giraud5@gmail.com",
+    description="Simple package to parse Cdiscount product prices",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/robingiraud/python-eval-robingiraud",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
